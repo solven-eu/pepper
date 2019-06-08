d9fe9a97addfd6d5383e29351a93af580d04c436
Benoit Lacelle <benoit.lacelle@gmail.com>
2019-06-08 09:48:17 +0200

    Upgrade avro to 1.9.0

--- serialization/spark/src/test/java/cormoran/pepper/spark/TestAvroBytesToStream.java
+++ serialization/spark/src/test/java/cormoran/pepper/spark/TestAvroBytesToStream.java
@@ -27,6 +27,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -56,7 +57,7 @@ import com.google.common.collect.Iterators;
 import com.google.common.primitives.Doubles;
 
 import cormoran.pepper.avro.AvroBytesToStream;
-import cormoran.pepper.avro.AvroStreamHelper;
+import cormoran.pepper.avro.AvroTranscodingHelper;
 
 public class TestAvroBytesToStream {
 	// https://github.com/FasterXML/jackson-dataformats-binary/blob/master/avro/src/test/java/com/fasterxml/jackson/dataformat/avro/MapTest.java
@@ -98,7 +99,7 @@ public class TestAvroBytesToStream {
 
 		List<? extends Map<String, ?>> avroStream =
 				new AvroBytesToStream().stream(new ByteArrayInputStream(baos.toByteArray()))
-						.map(AvroStreamHelper.toJavaMap())
+						.map(AvroTranscodingHelper.toJavaMap())
 						.collect(Collectors.toList());
 
 		Assert.assertEquals(1, avroStream.size());
@@ -114,20 +115,22 @@ public class TestAvroBytesToStream {
 				"doc",
 				"namespace",
 				false,
-				Arrays.asList(new Field("Currency", Schema.create(Type.STRING), null, Schema.NULL_VALUE)));
+				Arrays.asList(new Field("Currency", Schema.create(Type.STRING), null, null)));
 
 		BiMap<String, String> mapping = ImmutableBiMap.of("ccy", "Currency");
 		InputStream stream = PepperSparkHelper.toAvro(outputSchema, Iterators.singletonIterator(row), mapping);
 
-		List<?> resultAsList =
-				new AvroBytesToStream().stream(stream).map(AvroStreamHelper.toJavaMap()).collect(Collectors.toList());
+		List<?> resultAsList = new AvroBytesToStream().stream(stream)
+				.map(AvroTranscodingHelper.toJavaMap())
+				.collect(Collectors.toList());
 
 		Assert.assertEquals(1, resultAsList.size());
 		Assert.assertEquals(ImmutableMap.of("Currency", "someValue"), resultAsList.get(0));
 	}
 
+	@Ignore("https://issues.apache.org/jira/browse/AVRO-2366")
 	@Test
-	public void testAvroToStream_doublearray() throws IOException {
+	public void testAvroToStream_doublearray_nullDefault() throws IOException {
 		// Avro works with Collection by default
 		GenericRowWithSchema row = new GenericRowWithSchema(new Object[] { Doubles.asList(1D, 2D, 3D) },
 				new StructType(new StructField[] { new StructField("doubleArray", DataTypes.StringType, true, null) }));
@@ -145,7 +148,37 @@ public class TestAvroBytesToStream {
 		InputStream stream = PepperSparkHelper.toAvro(outputSchema, Iterators.singletonIterator(row), mapping);
 
 		List<?> resultAsList = new AvroBytesToStream().stream(stream)
-				.map(AvroStreamHelper.toJavaMap(ImmutableMap.of("DoubleArray", new double[0])))
+				.map(AvroTranscodingHelper.toJavaMap(ImmutableMap.of("DoubleArray", new double[0])))
+				.collect(Collectors.toList());
+
+		Assert.assertEquals(1, resultAsList.size());
+		Map<?, ?> singleOutput = (Map<?, ?>) resultAsList.get(0);
+		Assert.assertEquals(ImmutableSet.of("DoubleArray"), singleOutput.keySet());
+		Assert.assertArrayEquals(new double[] { 1D, 2D, 3D },
+				(double[]) singleOutput.values().iterator().next(),
+				0.0001D);
+	}
+
+	@Test
+	public void testAvroToStream_doublearray() throws IOException {
+		// Avro works with Collection by default
+		GenericRowWithSchema row = new GenericRowWithSchema(new Object[] { Doubles.asList(1D, 2D, 3D) },
+				new StructType(new StructField[] { new StructField("doubleArray", DataTypes.StringType, true, null) }));
+
+		Schema outputSchema = Schema.createRecord("testSchema",
+				"doc",
+				"namespace",
+				false,
+				Arrays.asList(new Field("DoubleArray",
+						Schema.createArray(Schema.create(Schema.Type.DOUBLE)),
+						null,
+						Collections.emptyList())));
+
+		BiMap<String, String> mapping = ImmutableBiMap.of("doubleArray", "DoubleArray");
+		InputStream stream = PepperSparkHelper.toAvro(outputSchema, Iterators.singletonIterator(row), mapping);
+
+		List<?> resultAsList = new AvroBytesToStream().stream(stream)
+				.map(AvroTranscodingHelper.toJavaMap(ImmutableMap.of("DoubleArray", new double[0])))
 				.collect(Collectors.toList());
 
 		Assert.assertEquals(1, resultAsList.size());
